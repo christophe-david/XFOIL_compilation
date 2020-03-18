757b12e0b61c4cb725e421e8496e1a56fb83b8ea
Christophe DAVID <chrdav81@free.fr>
2020-03-18 06:03:43 +0100

    Added test

---
+++ tests/test_xfoil.py
@@ -0,0 +1,111 @@
+import os
+import os.path as pth
+import platform
+import subprocess
+import numpy as np
+from numpy.testing import assert_allclose
+
+POLAR_RESULT = "polar_result.txt"
+
+
+def test_xfoil_full_iterations():
+    """
+    Tests a XFOIL polar with 200 iterations per point
+
+    All points that can converge are expected to be there
+    """
+    if pth.exists(POLAR_RESULT):
+        os.remove(POLAR_RESULT)
+
+    # Get built xfoil path
+    xfoil_path = pth.join(pth.dirname(__file__), pth.pardir, "bin", "xfoil")
+
+    with open(
+        pth.join(pth.dirname(__file__), "test_session_200it.txt")
+    ) as session_inputs:
+        with open(
+            pth.join(pth.dirname(__file__), "session_200it.log"), "w"
+        ) as session_outputs:
+            if platform.system() == "Windows":
+                # Reference test using official build
+                subprocess.run(
+                    ["xfoil.exe"], stdin=session_inputs, stdout=session_outputs
+                )
+            elif platform.system() == "Darwin":
+                # Test macOS build
+                subprocess.run(
+                    [xfoil_path], stdin=session_inputs, stdout=session_outputs
+                )
+
+    result = np.genfromtxt(POLAR_RESULT, skip_header=12)
+    alpha = result[:, 0]
+    CL = result[:, 1]
+
+    reference = np.genfromtxt(
+        pth.join(pth.dirname(__file__), "polar_result_ref.txt"), skip_header=12
+    )
+    alpha_ref = reference[:, 0]
+    CL_ref = reference[:, 1]
+
+    assert_allclose(
+        alpha, alpha_ref, atol=1e-4,
+    )
+    assert_allclose(
+        CL, CL_ref, atol=1e-4,
+    )
+
+
+def test_xfoil_reduced_iterations():
+    """
+    Tests a XFOIL polar with 20 iterations per point
+
+    Some points are expected to not converge and be missing in the result polar.
+    It is checked that:
+    - results of computed point have same CL value as with large iteration count.
+    - enough points are computed
+    - max CL is obtained
+    """
+
+    if pth.exists(POLAR_RESULT):
+        os.remove(POLAR_RESULT)
+
+    # Get built xfoil path
+    xfoil_path = pth.join(pth.dirname(__file__), pth.pardir, "bin", "xfoil")
+
+    with open(
+        pth.join(pth.dirname(__file__), "test_session_20it.txt")
+    ) as session_inputs:
+        with open(
+            pth.join(pth.dirname(__file__), "session_20it.log"), "w"
+        ) as session_outputs:
+            if platform.system() == "Windows":
+                # Reference test using official build
+                subprocess.run(
+                    ["xfoil.exe"], stdin=session_inputs, stdout=session_outputs
+                )
+            elif platform.system() == "Darwin":
+                # Test macOS build
+                subprocess.run(
+                    [xfoil_path], stdin=session_inputs, stdout=session_outputs
+                )
+
+    result = np.genfromtxt(POLAR_RESULT, skip_header=12)
+    alpha = result[:, 0]
+    CL = result[:, 1]
+
+    reference = np.genfromtxt(
+        pth.join(pth.dirname(__file__), "polar_result_ref.txt"), skip_header=12
+    )
+    alpha_ref = reference[:, 0]
+    CL_ref = reference[:, 1]
+
+    idx_ref = np.isin(alpha_ref, alpha)
+    assert_allclose(
+        CL, CL_ref[idx_ref], atol=1e-4,
+    )
+
+    assert len(CL) > 35
+    assert np.max(alpha) > 18.0
+    assert_allclose(
+        np.max(CL), np.max(CL_ref), atol=1e-4,
+    )
